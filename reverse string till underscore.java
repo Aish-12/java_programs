f05b19cb9348481fd78a1264423b6511c1e3d899
Aish-12 <83743568+Aish-12@users.noreply.github.com>
2022-07-10 07:54:38 +0530

    Update reverse string till underscore.java

--- reverse string till underscore.java
+++ reverse string till underscore.java
@@ -1,1 +1,26 @@
-import 
+import java.util.*;
+public class Hello {
+
+    public static void main(String[] args) {
+		//Your Code Here
+		Scanner sc = new Scanner(System.in);
+		String s = sc.nextLine();
+		int n = s.indexOf("_");
+		if(n==-1){
+		    System.out.println(new StringBuffer(s).reverse().toString());
+		}
+		else if(n==0){
+		    System.out.println(s);
+		}
+		else{
+		    if(n==s.length()-1){
+		        String[] s2 = s.split("_");
+		        System.out.println(new StringBuffer(s2[0]).reverse().toString()+"_");
+		    }
+		    else{
+		        String[] s2 = s.split("_");
+		        System.out.println(new StringBuffer(s2[0]).reverse().toString()+"_"+s2[1]);
+		    }
+		}
+	}
+}
