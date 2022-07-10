a50c47ecfd582b8095d3f800a2433167c9cb866c
Aish-12 <83743568+Aish-12@users.noreply.github.com>
2022-07-10 07:18:11 +0530

    Update leap_year.java

--- leap_year.java
+++ leap_year.java
@@ -5,7 +5,7 @@ public class Hello {
 		//Your Code Here
 		Scanner sc=new Scanner(System.in);
     int year = sc.nextInt();
-    if((year %400==0 && year %100!=0 ) || year %4==0){
+    if((year %400==0) || ((year %100!=0 ) && (year %4==0))){
       System.out.println("yes");
     }
     else{
