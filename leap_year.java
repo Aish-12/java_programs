a51240496235d10d8ef239a233c3d24415c90726
Aish-12 <83743568+Aish-12@users.noreply.github.com>
2022-07-10 07:15:19 +0530

    Create leap_year.java

---
+++ leap_year.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Hello {
+
+    public static void main(String[] args) {
+		//Your Code Here
+		Scanner sc=new Scanner(System.in);
+    int year = sc.nextInt();
+    if((year %400==0 && year %100!=0 ) || year %4==0){
+      System.out.println("yes");
+    }
+    else{
+      System.out.println("no");
+    }
